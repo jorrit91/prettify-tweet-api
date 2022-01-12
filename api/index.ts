f91056da8fabf6f37c3209d91829b604ddf0819e
Jorrit Tempelman <tempelmanjorrit@gmail.com>
2022-01-12 21:50:14 +0100

    add cors

--- api/index.ts
+++ api/index.ts
@@ -2,16 +2,15 @@ import { MercuriusOptions } from "mercurius"
 import { options } from "./graphql"
 import fastify from "fastify"
 import mercurius from "mercurius"
+import cors from "fastify-cors"
 import * as dotenv from "dotenv"
 
 dotenv.config()
 
 const app = fastify()
-app.register(mercurius, options)
-
-app.get("/", async function (req, reply) {
-  const query = "{ add(x: 2, y: 2) }"
-  return reply.graphql(query)
+app.register(cors, {
+  origin: "*",
+  methods: ["GET", "POST", "OPTIONS"],
 })
-
+app.register(mercurius, options)
 app.listen(4000)
