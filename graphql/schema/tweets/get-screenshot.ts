f1f54d9329756c048f3096d0b90ea1ca6f303fce
Jorrit Tempelman <tempelmanjorrit@gmail.com>
2022-02-08 19:32:04 +0100

    remove puppeteer

--- graphql/schema/tweets/get-screenshot.ts
+++ graphql/schema/tweets/get-screenshot.ts
@@ -1,9 +1,6 @@
 import { extendType, stringArg } from "nexus"
-import puppeteer from "puppeteer"
-import { getS3Client, uploadFileGetTemporaryUrl } from "../../../lib/s3"
-import * as fs from "fs"
-import { getScreenshot as getPuppeteerScreenshot } from "../../../lib/get-screenshot"
 import fetch from "node-fetch"
+import { uploadFileGetTemporaryUrl } from "../../../lib/s3"
 
 export const getScreenshot = extendType({
   type: "Mutation",
@@ -20,17 +17,10 @@ export const getScreenshot = extendType({
 
         try {
           const response = await fetch(
-            // `https://api.rasterwise.com/v1/get-screenshot?apikey=${process.env.GET_SCREENSHOT_KEY}&url=https://2446-2a02-a44d-7142-1-f41f-15e0-f1a2-f3d2.ngrok.io/screenshot?id=1490055796704497665&color=dark&layout=auto&element=#preview-parent`
-            `https://api.rasterwise.com/v1/get-screenshot?apikey=${process.env.GET_SCREENSHOT_KEY}&url=https://google.com`
+            `https://api.rasterwise.com/v1/get-screenshot?apikey=${process.env.GET_SCREENSHOT_KEY}&url=https://2446-2a02-a44d-7142-1-f41f-15e0-f1a2-f3d2.ngrok.io/screenshot?id=1490055796704497665&color=dark&layout=auto&element=#preview-parent`
           ).then((response) => response.json())
           const peter = response.screenshotImage
           const image = await fetch(peter).then((response) => response.buffer())
-          console.log(image)
-          // const screenshot = await getPuppeteerScreenshot({
-          //   tweetId,
-          //   color,
-          //   layout,
-          // })
 
           const date = new Date()
           const filename = `prettify-tweet-${tweetId}-${color}-${layout}-${date.getTime()}.${imageType}`
@@ -39,8 +29,7 @@ export const getScreenshot = extendType({
             mimetype: `image/${imageType}`,
             Key: `screenshots/${filename}`,
           })
-          console.log("peter")
-          console.log({ url })
+
           return { url, filename }
         } catch (err) {
           throw new Error(`❌ Error: ${err.message}`)
