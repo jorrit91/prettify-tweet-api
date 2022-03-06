2272337ce124fe8ce9806ce7764db67489d4a0c1
Jorrit Tempelman <tempelmanjorrit@gmail.com>
2022-03-06 20:51:55 +0100

    adjust device factor

--- lib/get-screenshot-image.ts
+++ lib/get-screenshot-image.ts
@@ -15,7 +15,7 @@ export async function getScreenshotImage({
     `${process.env.CLIENT_URL}/screenshot?id=${tweetId}&color=${color}&layout=${layout}`
   )
   const rasterwise: any = await fetch(
-    `https://api.rasterwise.com/v1/get-screenshot?apikey=${process.env.GET_SCREENSHOT_KEY}&url=${encodedUrl}&element=%23preview-parent > div > div&devicefactor=2&forcetr=true`
+    `https://api.rasterwise.com/v1/get-screenshot?apikey=${process.env.GET_SCREENSHOT_KEY}&url=${encodedUrl}&element=%23preview-parent > div > div&devicefactor=3&forcetr=true`
   ).then((response) => response.json())
   const imageUrl = rasterwise.screenshotImage
   const image = await fetch(imageUrl).then((response) => response.buffer())
